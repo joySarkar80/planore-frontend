3a1986feda6cfbc9bff257dfb291c57efe9ba5a7
joySarkar <joysarkar98600@gmail.com>
2026-05-05 17:51:22 +0600

    next config file edited and .env created

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+  },
+  /* 
+  async rewrites() {
+    return [
+      {
+        source: '/server/:path*',
+        destination: 'http://localhost:5000/api/:path*', 
+      },
+    ];
+  }, 
+  */
+};
+
+export default nextConfig;
